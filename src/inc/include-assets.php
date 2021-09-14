262c6d71305c0680bf5c77b26dcc12ef95880cc3
koudai0024 <iwai.0731v@gmail.com>
2021-09-14 16:37:35 +0900

    🍱 javascriptの呼び出し設定

--- src/inc/include-assets.php
+++ src/inc/include-assets.php
@@ -6,12 +6,21 @@ class IncludeAssets
     public function __construct()
     {
         add_action('wp_enqueue_scripts', array($this, 'includeCss'));
+        add_action('wp_enqueue_scripts', array($this, 'includeJs'));
     }
     
     public function includeCss()
     {
         if (! is_admin()) {
             wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1');
+        }
+    }
+
+    public function includeJs()
+    {
+        if (! is_admin()) {
+            wp_deregister_script('jquery');
+            wp_enqueue_script('main-sctipt', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '1', true);
         }
     }
 }
